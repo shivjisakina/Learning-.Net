9f2338cf302e4dced98b6a0e004db1e10a92f7b0
Sakina Shivji <shivjisakina@gmail.com>
2017-08-20 16:25:12 -0400

    created Equipment Model and dynamically added information to Views

--- CharacterSheetApp/Controlllers/HomeController.cs
+++ CharacterSheetApp/Controlllers/HomeController.cs
@@ -12,3 +12,22 @@ namespace CharacterSheetApp.Controllers
     }
   }
 }
+
+/* Using the Equipment Model:
+
+using Microsoft.AspNet.Mvc;
+using CharacterSheetApp.Models;
+using System;
+
+namespace CharacterSheetApp.Controllers
+{
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      var model = new Equipment();
+      model.Name = "Shield";
+      return View("Index", model);
+    }
+  }
+} */
---
+++ CharacterSheetApp/Models/Equipment.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CharacterSheetApp.Models
+{
+  public class Equipment
+  {
+    public String Name;
+  }
+}
